c7c8d1e83d247b442114b7bc4821e2736fad5ddf
rohit5681 <85388416+rohit5681@users.noreply.github.com>
2023-04-18 20:54:28 +0530

    Add files via upload

--- jobs.js
+++ jobs.js
@@ -1,15 +1,24 @@
-//Require the existing Express
-const express = require("express");
-//Create a Local Router
-const router = express.Router();
-//Require the Passport Module
-const passport = require("passport");
-
-//Require Jobs Controller
-const jobsController = require("../controllers/jobs_controller");
-
-//Access the Jobs Controller's portal() Function @ '/' route.
-router.get("/", passport.checkAuthentication, jobsController.portal);
-
-//Export the Router
-module.exports = router;
+{
+	try {
+		const card = document.getElementsByClassName("card")[0];
+		const jobs = card.getAttribute("data-jobs");
+		//Create Job Cards Dynamically
+		for (let job of JSON.parse(jobs)) {
+			let span1 = document.createElement("span");
+			let span2 = document.createElement("span");
+			const newCard = card.cloneNode(true);
+			newCard.style.display = "inline-block";
+			newCard.querySelector(".card-header").textContent =
+				job.company_name + " 🚀";
+			newCard.querySelector(".card-title").textContent = job.job_title;
+			span1.textContent = `: ${job.interview_date}`;
+			newCard.querySelectorAll(".card-text")[0].appendChild(span1);
+			span2.textContent = `: ${job.location}`;
+			newCard.querySelectorAll(".card-text")[1].appendChild(span2);
+			newCard.querySelector("a").href = job.url;
+			document.getElementsByClassName("jobPage")[0].appendChild(newCard);
+		}
+	} catch (error) {
+		console.log("Error in Fetching the Jobs ❌", error);
+	}
+}
